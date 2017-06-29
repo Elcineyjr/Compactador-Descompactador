585d1aa7e1c5b798468fed29da7dc1d855492760
Elciney Jr <elcineyjr123@gmail.com>
2017-06-28 23:10:55 -0300

    bloco de notas e definiçao de algumas funçoes

---
+++ ListaArvore.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Arvore.h"
+
+
+//funçao que gera a lista com todos os caracteres
+//funçao que transforma as duas primeiras celulas em uma arvore
+//funçao que insere a arvore na lista (ordenada)
+//funçao que retorna a unica arvore que sobrou na lista
