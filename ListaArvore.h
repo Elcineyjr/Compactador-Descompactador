fdf5e21795800b9ec095e0a7044758673ea637c4
Elciney Jr <elcineyjr123@gmail.com>
2017-07-05 20:12:18 -0300

    definindo funçoes

--- ListaArvore.h
+++ ListaArvore.h
@@ -1,5 +1,18 @@
-
+#ifndef TRAB2_LISTAARVORE_H
+#define TRAB2_LISTAARVORE_H
 
 typedef struct lista ListaArvore;
 
 typedef struct celula Celula;
+
+ListaArvore* cria_lista();
+
+int lista_vazia(ListaArvore* lista);
+
+ListaArvore* lista_insere_celula_ordenada(ListaArvore* lista, Celula* celula);
+
+ListaArvore* gera_lista_caractes(int vetor[], int tamanho_vetor);
+
+void lista_imprime(ListaArvore* lista);
+
+#endif //TRAB2_LISTAARVORE_H
