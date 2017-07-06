ff7327e23b31d33ee3fd528476ea780fbf7b131c
Elciney Jr <elcineyjr123@gmail.com>
2017-07-06 19:49:37 -0300

    limpando funçoes que nao serao usadas

--- Arvore.h
+++ Arvore.h
@@ -3,15 +3,6 @@
 
 typedef struct arvore Arv;
 
-
-/*
-*
-*Esse arquivo só foi copiado de um exercicio da patricia
-*Nao serao usadas todas as funçoes que estão aqui
-*
-*/
-
-
 //Cria uma árvore vazia
 Arv* arv_criavazia (void);
 
@@ -21,28 +12,6 @@ Arv* arv_cria (char c, int peso, Arv* e, Arv* d);
 
 //libera o espaço de memória ocupado pela árvore a
 Arv* arv_libera (Arv* a);
-
-//retorna true se a árvore estiver vazia e false
-//caso contrário
-int arv_vazia (Arv* a);
-
-//indica a ocorrência (1) ou não (0) do caracter c
-int arv_pertence (Arv* a, char c);
-
-//imprime as informações dos nós da árvore
-void arv_imprime (Arv* a);
-
-//retorna o pai de um dado no
-Arv* arv_pai (Arv* a, char c);
-
-//retorna a quantidade de folhas de uma arvore binaria
-int folhas (Arv* a);
-
-//retorna o numero de ocorrencias de um dado caracter na arvore
-int ocorrencias (Arv* a, char c);
-
-//retorna o campo informacao de um dado no
-char info (Arv* a);
 
 int arv_get_peso(Arv* arvore);
 
