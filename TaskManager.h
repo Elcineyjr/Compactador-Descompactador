efafb6a3bfacb1d612a325f8661a0e16e5da3a51
Elciney Jr <elcineyjr123@gmail.com>
2017-07-12 04:13:14 -0300

    declarando funçao imprime para testes

--- TaskManager.h
+++ TaskManager.h
@@ -7,6 +7,8 @@
 *Pre-condiçao: Arquivo aberto e vetor existente
 *Pos-condiçao: arquivo lido e frequencia dos caracteres inseridos no respectivos indice no vetor
 */
-int* gera_vetor_ascii_frequencia(FILE* arquivo, int* vetor)
+void gera_vetor_ascii_frequencia(FILE* arquivo, int* vetor);
+
+void vetor_imprime(int* vetor);
 
 #endif //TRAB2_TASKMANAGER_H
