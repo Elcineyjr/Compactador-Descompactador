45ad512b06411b42b9c6880cb8be58ce42591ff7
Elciney Jr <elcineyjr123@gmail.com>
2017-07-12 03:50:57 -0300

    Problema na declaraçao de FILE*

---
+++ TaskManager.h
@@ -0,0 +1,12 @@
+#ifndef TRAB2_TASKMANAGER_H
+#define TRAB2_TASKMANAGER_H
+
+/*
+*Input: -Arquivo
+*Output: ponteiro pro primeiro indice de um vetor
+*Pre-condiçao: Arquivo aberto e vetor existente
+*Pos-condiçao: arquivo lido e frequencia dos caracteres inseridos no respectivos indice no vetor
+*/
+int* gera_vetor_ascii_frequencia(FILE* arquivo, int* vetor)
+
+#endif //TRAB2_TASKMANAGER_H
