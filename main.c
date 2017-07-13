c19fc3d0e3172e9dc6c0b795a97c848539f57b71
Elciney Jr <elcineyjr123@gmail.com>
2017-07-12 22:25:54 -0300

    testes das funçoes novas

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 *********************************
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include "Arvore.h"
 #include "ListaArvore.h"
 #include "TaskManager.h"
@@ -21,11 +22,28 @@ int main()
     FILE* arquivo;
     int vetor[255];
 
-    arquivo = fopen("entrada.txt", "r");
-
+    arquivo = fopen("jobson.txt", "r");
+    
+    if(arquivo == NULL)
+    {
+        printf("Falha na abertura do arquivo! Abortando execução do programa!\n");
+        exit(1);
+    }
+    
     gera_vetor_ascii_frequencia(arquivo, vetor);
 
     vetor_imprime(vetor);
 
+    ListaArvore* nova_lista = gera_lista_caractes(vetor, 255);
+    printf("11111111111\n");
+
+    //lista_imprime(nova_lista);
+
+    Arv* nova_arv = gera_arvore_huffman(nova_lista);
+    printf("22222222222\n");
+
+    arv_imprime(nova_arv);
+
+    fclose(arquivo);
     return 0;
 }
