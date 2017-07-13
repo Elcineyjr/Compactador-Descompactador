6a68dcc4df963716d9578172c6a6752e3c91b7d7
Elciney Jr <elcineyjr123@gmail.com>
2017-07-12 22:23:48 -0300

    definindo funçoes

--- Arvore.h
+++ Arvore.h
@@ -10,11 +10,15 @@ Arv* arv_criavazia (void);
 //com subárvore esquerda e e subárvore direita d
 Arv* arv_cria (char c, int peso, Arv* e, Arv* d);
 
+Arv* arv_cria_no_comum(int peso, Arv* e, Arv* d);
+
 //libera o espaço de memória ocupado pela árvore a
 Arv* arv_libera (Arv* a);
 
 int arv_get_peso(Arv* arvore);
 
 char arv_get_char(Arv* arvore);
+
+void arv_imprime(Arv* arvore);
 
 #endif //TRAB2_ARVORE_H
