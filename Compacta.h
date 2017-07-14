bce567b43cc096c9ee17b183675ecc5f37a092fd
Elciney Jr <elcineyjr123@gmail.com>
2017-07-14 19:54:29 -0300

    Programa compactador

---
+++ Compacta.h
@@ -0,0 +1,10 @@
+#include "TADs/Arvore.h"
+#include "TADs/ListaArvore.h"
+#include "TADs/Vetor.h"
+#include "TADs/ListaBits.h"
+#ifndef TRAB2_COMPACTA_H
+#define TRAB2_COMPACTA_H
+
+
+
+#endif //TRAB2_COMPACTA_H
