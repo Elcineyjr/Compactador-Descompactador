5886e634cf8e6e997f42efbe8db91ca79e763188
Elciney Jr <elcineyjr123@gmail.com>
2017-07-16 02:22:32 -0300

    Iniciando a compactaçao do arquivo

--- Compacta.c
+++ Compacta.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Compacta.h"
+#define tamanho_ASCII 255
 
+void compacta_arquivo(FILE* arquivo){
+    int vetor[tamanho_ASCII];
+    ListaArvore* lista_caracteres;
+    Arv* arvore_otima;
+    
+    gera_vetor_ascii_frequencia(arquivo, vetor);
+    lista_caracteres = gera_lista_caractes(vetor, tamanho_ASCII);
+    arvore_otima = gera_arvore_huffman(lista_caracteres);
+    
+    //********** TERMINAR FUNÇAO ***************
+}
